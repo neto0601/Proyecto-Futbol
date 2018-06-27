6463c7cba1caf3b52fe656a51f685956344a2c6a
Ernesto <Ernesto@DESKTOP-CG63FUL>
2018-06-26 21:40:21 -0600

    Agregando los demas botones en ingresoJugadores

--- src/sistemafutbol/sqlJugadores.java
+++ src/sistemafutbol/sqlJugadores.java
@@ -3,14 +3,16 @@ package sistemafutbol;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
 public class sqlJugadores extends conexion{
     
-    private jugadores js;
+    private jugadores jo;
     
     public boolean registrar(jugadores jds){
         PreparedStatement ps = null;
@@ -30,6 +32,70 @@ public class sqlJugadores extends conexion{
             Logger.getLogger(sqlJugadores.class.getName()).log(Level.SEVERE, null, ex);
             return false;
         }
+    }
+    
+    public boolean Eliminar(String carnet){
+        PreparedStatement ps = null;
+        Connection con = getConexion();
+            
+        try {
+            ps = con.prepareStatement("DELETE FROM jugadores WHERE carnet=?");
+            ps.setString(1, carnet);
+            ps.execute();
+            return true;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(sqlJugadores.class.getName()).log(Level.SEVERE, null, ex);
+           return false; 
+        }
+        
+    }
+    
+    public boolean updateMascota(jugadores jo){
+    
+         PreparedStatement ps = null;
+        Connection con = getConexion();
+
+        String sql = "UPDATE mascotas SET nombres = ?, apellidos = ?, carrera= ? WHERE carnet = ?";
+        try {
+            ps = con.prepareStatement(sql);
+            ps.setString(1, jo.getNombres());
+            ps.setString(2, jo.getApellidos());
+            ps.setString(3, jo.getCarrera());
+            ps.setString(4, jo.getCarnet());
+            
+            ps.execute();
+            return true;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(sqlJugadores.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    
+    
+    }
+    
+     public jugadores buscarJugador(String carnet) {
+    
+         try {
+         jo = new jugadores();
+         Connection con = getConexion();
+         Statement stmt = con.createStatement();
+         ResultSet rs = stmt.executeQuery("SELECT * FROM jugadores where carnet = " + carnet);
+         while(rs.next()){
+             jo.setCarnet(rs.getString("carnet"));
+             jo.setNombres(rs.getString("nombres"));
+             jo.setApellidos(rs.getString("apellidos"));
+             jo.setCarrera(rs.getString("carrera"));
+             
+         }
+        } catch (Exception e) {
+            
+            System.out.println("Error " + e.getMessage());
+        }
+         return jo;
+            
+        
     }
     
     
